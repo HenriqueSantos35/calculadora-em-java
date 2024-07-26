f922dc82da94d2926d6b7c42a91804246468a8b7
Allie Henrique <164886832+HenriqueSantos35@users.noreply.github.com>
2024-07-26 20:02:36 -0300

    lógica corrigida

--- calculadora.java
+++ calculadora.java
@@ -1,27 +1,54 @@
 import java.util.Scanner;
 
-/**
- * calculadora
- */
 public class calculadora {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        Double valor1;
-        Double valor2;
+        Double valorUm;
         String operacao;
+        Double valorDois;
 
         System.out.println("Digite o primeiro valor: ");
-        valor1 = scanner.nextDouble();
+        valorUm = input.nextDouble();
 
-        System.out.println("Digite a operação(+, -, /, *): ");
-        operacao = scanner.next();
+        System.out.println("Escolha a operação( +, -, *, / ): ");
+        operacao = input.next();
 
         System.out.println("Digite o segundo valor: ");
-        valor2 = scanner.nextDouble();
+        valorDois = input.nextDouble();
 
-        
+        Double resultado = realizarCalculo(valorUm, valorDois, operacao);
+        if (resultado != null) {
+            System.out.println("Resultado: " + resultado);
+        }
+        input.close();
+    }
 
+    public static Double realizarCalculo(Double valorUm, Double valorDois, String operacao) {
+        Double respostaCalculo = 0.0;
+
+        switch (operacao) {
+            case "+":
+                respostaCalculo = valorUm + valorDois;
+                break;
+            case "-":
+                respostaCalculo = valorUm - valorDois;
+                break;
+            case "*":
+                respostaCalculo = valorUm * valorDois;
+                break;
+            case "/":
+                if (valorDois != 0) {
+                    respostaCalculo = valorUm / valorDois;
+                } else {
+                    System.out.println("Erro: Divisão por zero não é permitida.");
+                    return null;
+                }
+                break;
+            default:
+                System.out.println("Operação inválida");
+                return null;
+        }
+        return respostaCalculo;
     }
 }
